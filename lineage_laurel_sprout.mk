8e60d04f887f4017b00c4393a36ab123619fb308
vishwajithkv <vishwajithkv@users.noreply.github.com>
2026-07-31 15:28:59 +0530

    laurel_sprout: Exclude default boot animation
    Keep only the resolution-specific animation to avoid duplicating the system asset.

--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -8,6 +8,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Use the device-specific boot animation.
+TARGET_EXCLUDE_DEFAULT_BOOTANIMATION := true
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
